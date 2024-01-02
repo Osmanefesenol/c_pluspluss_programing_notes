6fb4ebdac38745c0b0e813a1022d018f27d3841a
futurecoderagency <116516471+Osmanefesenol@users.noreply.github.com>
2024-01-02 17:03:22 +0300

    Language Fundamentals
    -variables
    -data types

---
+++ Language Fundamentals/Language Fundamentals/Language Fundamentals.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    /*std::cout << "Hello C++!\n";
+
+    //VARİABLES (Değişkenler ve Tanımlama)
+    
+    int a = 20;
+    int b = 50;
+    
+    int c = 30, d = 40;
+
+    std::cout << a << b << c << d;
+
+    //İDENTİFİERS (DEĞİŞKEN İSİMLERİ VE BELİRLEYİCİLER)
+
+    int yas;
+    int yil;
+    int gun; // Bu tarz sayı yerine yazı değerleri de verilebilir.
+
+    // Bazı durumlarda bu tanımlama geçersiz olur. 
+    // Bu durumlar sayının başta olma durumu ve reserved keyword, yani int, cout, cin gibi ifadeler.
+    // int 7yas, int cout gibi durumlar kısaca. Onun dışında sayı yazı metninin içinde başta olmamak şartıyla her türlü kullanılır.
+
+
+    //VARİABLE TYPES (DEĞİŞKEN TİPLERİ)
+
+    float pi = 3.14; //float tipi ondalıklı sayılar için kullanılır.
+    char c = 'x'; //char ifadesi bir karakter belirtilirken kullanılır.
+    int a = 3; //int tipi tam sayılar için kullanılır. Ondalıklı yazsak bile sadece sayının tam kısmını alır.
+
+    //Örnek*/
+
+    /*string isim = "Ali";
+    int yas = 22;
+
+    cout << "Bir zamanlar " << isim << " isminde bir genc varmis." << endl
+        << isim << " " << yas << " yasindaymis" << endl;
+
+    string ad = "Mehmet";
+    int yasi = 25;
+
+    cout << ad << " ismini sever ancak bulundugu " << yasi << " yasin sevmezmis. Daha yasli olmak istermis./n"; */
+
+    // VERİ TİPLERİ
+
+    /*string isim = "Kaan Kaya";
+    int dogumYili = 1980;
+    float sayisalNotOrtalamasi = 3.47f;   //sayının yanına f yazarak float tipinde tutmasını sağlarız. Aksi durumda double olarak tutar ve bu da RAM'dan yer.
+    double pi = 3.14734627883265725;
+    bool sinifiGectiMi = true;
+    char harfNotu = 'B';
+    
+    cout << "Merhaba Sayin " << isim << endl;
+    cout << "Dogum Yili: " << dogumYili << endl;
+    cout << "Sayisal Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+    cout << "Sinifi Gectiniz mi: " << sinifiGectiMi << endl;
+    cout << "Not Ortalamasi: " << harfNotu << endl;
+
+    return 0;   */
+
+    // ARİTMETİK İŞLEMLER
+
+    /*string isim = "Kaan Kaya";
+    int dogumYili = 1980;
+    float sayisalNotOrtalamasi = 3.47f;   
+    double pi = 3.14734627883265725;
+    bool sinifiGectiMi = true;
+    char harfNotu = 'B';
+    int quizNot1 = 78;    // bu iki nottan biri float yapılsada yine küsüratlı değer çıkar.
+    int quizNot2 = 53;
+    float quizOrtalama = (float)(quizNot1 + quizNot2) / 2;  //Küsüratlı çıkması için parantez içinde float kullanılabilir.
+
+    cout << "Merhaba Sayin " << isim << endl;
+    cout << "Dogum Yili: " << dogumYili << endl;
+    cout << "Sayisal Not Ortalamasi: " << sayisalNotOrtalamasi << endl;
+    cout << "Sinifi Gectiniz mi: " << sinifiGectiMi << endl;
+    cout << "Not Ortalamasi: " << harfNotu << endl;
+    cout << "Quizler ortalamasi: " << quizOrtalama << endl;   
+
+
+    int x = 40;
+    int y = 30;
+    int z = 40 % 30;    // % : mod (modulus) operatoru, sadece kalanı verir.
+
+    cout << z << endl;
+
+    return 0;     */
+
+       // KULLANICIDAN VERİ (İNPUT) ALMA
+
+    /*string isim;
+    int en, boy, alan;
+
+    cout << "Lutfen adinizi yaziniz: ";     // user friendly
+    cin >> isim;
+    cout << "Merhaba " << isim << ", Geomtrik hesaplama programina hosgeldin!\n\n";
+    cout << "Lutfen dikdortgenin enini giriniz: ";
+    cin >> en;
+    cout << "Lutfen dikdortgenin boyunu giriniz: ";
+    cin >> boy;
+
+    //hesaplama işlemi
+
+    alan = en * boy;
+
+    cout << "Sayin " << isim << ", isleminizin sonucu: " << alan << endl;  */
+
+    
+
+
+
+
+
+
+}
+
+
